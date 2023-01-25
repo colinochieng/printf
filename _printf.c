d480dbc882adb668e27dd0a63e62564419a16494
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-01-26 00:34:04 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -4,15 +4,19 @@
 int int _printf(const char *format, ...)
 {
 	unsigned int i;
+	char *buffer, *str;
 	va_list arg_list;
+
 	if (format != NULL)
 	{
 		va_start(arg_list, format);
+		
 
 		for (i = 0; format && format[i] != ' '; i++)
 		{
 			if (format[0] == % && format[1] == ' ')
 				return (-1);
+			
 
 			
 		
