fdd8fbbd8546ecf644ed6d373af570a42de48179
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-01-25 23:10:54 +0300

    Create specifiers.c

---
+++ specifiers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+char *specifiers(var_list arg_list, const char format)
+{
+	int i;
+	char *str_ptr;
+
+	spc_holder value[] = {
+		{spe_char, 'c'},
+		{spe_string, 's'},
+		{spe_percent, '%'}
+	};
+
+	for (i = 0; i < 3 && value[i].point != format; i++)
+		;
+
+	str_ptr = value[i].func_ptr(format);
+	
+	return (str_ptr);
+}
