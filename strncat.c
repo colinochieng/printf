637bd192bd2cc49a6cb2749c9f6abaafab3ed59b
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-01-26 00:59:02 +0300

    Create strncat.c

---
+++ strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+*_strncat - joins two strings
+*@dest: first string
+*@src: second string
+*@n: bytes of n to be allocates to dest
+*Return: joined string
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	;
+
+	for (j = 0; src[j] != '\0' && j < n; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
