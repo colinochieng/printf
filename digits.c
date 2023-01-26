b59a5e56478e53775afbbafae99be6bc85fda0a8
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-01-27 00:15:07 +0300

    Create digits.c

---
+++ digits.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+*sum_up - counts number of digits in integers
+*@n: passed interger digit
+*Return: digits count
+*/
+int sum_up(int n)
+{
+	unsigned int i, j = 0;
+/*verifies whether the input integer is negative to be converted to positive*/
+	if (n < 0)
+		i = j * -1;
+	else
+		i = j;
+
+/*division carried out several times until the quotient is zero*/
+	for(; i != 0; j++)
+		i /= 10;
+  
+	return (j);
+}
+  
