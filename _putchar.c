d97382262679af88dc42f1e5a836adcd908570f5
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-01-26 18:34:45 +0300

    Create _putchar.c

---
+++ _putchar.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+*_putchar - writes a single character
+*as an argument to standard output
+*@c: character to be written in stdout
+*Return: 1
+*/
+int _putchar(char c)
+{
+	static int m;
+	static char buffer[BUFSIZE];
+
+	if (m >= BUFSIZE || c == EOF)
+	{
+		write(1, &buffer, m);
+
+		i = 0;
+	}
+	if (c != EOF)
+	{
+		buffer[m] = c;
+		m++;
+	}
+	return (1);
+}
