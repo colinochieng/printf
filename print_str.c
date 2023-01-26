4fa50e5f0315e7d7532aa31b455bb6be7c259e17
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-01-26 18:04:51 +0300

    Create print_str.c

---
+++ print_str.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*print_str - outputs string
+*@arg: variable _printf's arguments
+*@func: points to struct
+*Return: string
+*/
+int print_str(va_list arg, flags_t *func)
+{
+	(void)func;
+	char *get_str(arg, char *);
+
+	if (get_str == NULL)
+		get_str = "(null)";
+
+	return (put_str(get_str));
+}
