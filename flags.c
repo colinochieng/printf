e3c74f1a777f0d18e844d74728a533a83190ab0b
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-01-26 18:53:06 +0300

    Create flags.c

---
+++ flags.c
@@ -0,0 +1,29 @@
+/**
+*flags - activates flags on _printf
+*@f: flag character
+*@func: struct pontier
+*Return: 1 or 0
+*/
+
+int flags(char f, indicator *func)
+{
+	int m = 0;
+
+	if (f == '+')
+	{
+		(*func).plus = 1;
+		m = 1;
+	}
+	else if (f == ' ')
+	{
+		(*func).space = 1;
+		m = 1;
+	}
+	else if (f == '#')
+	{
+		(*func).hash = 1;
+		m = 1;
+	}
+
+	return (m);
+}
