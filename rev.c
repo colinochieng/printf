1cfa341aaa1c03745adfe367563b6f6098c789dd
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-01-27 03:17:40 +0300

    Create rev.c

---
+++ rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+*rev - reverses a string
+*@: string arguments
+*Return: total printed characters
+*/
+int rev(va_list arg)
+{
+	char *str;
+	int i, digits, j = 0;
+
+	str = va_arg(arg, char *);
+	if (str == NULL)
+		str = "(nil)";
+
+	i = _strlen(str);
+	j = i - 1;
+
+	while (j >= 0)
+	{
+		_putchar(s[j]);
+		j--;
+		digits++;
+	}
+
+	return (digits);
+}
