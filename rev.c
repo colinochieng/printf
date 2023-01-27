f6896e2842d773130f41c62fa0a405150647774c
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-01-27 04:09:29 +0300

    Update rev.c

--- rev.c
+++ rev.c
@@ -5,14 +5,13 @@
 *@: string arguments
 *Return: total printed characters
 */
-int rev(va_list arg,  indicator *func_ptr)
+int rev(va_list arg)
 {
 	char *str;
 	int i, digits, j = 0;
 
 	str = va_arg(arg, char *);
 
-	void(func_ptr);
 
 	if (str == NULL)
 		str = "(nil)";
