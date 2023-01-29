2b3bfd7cf839e9f1580a42a3a6d4f7fa98f586f0
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-01-30 00:20:10 +0300

    Update rev.c

--- rev.c
+++ rev.c
@@ -9,11 +9,11 @@
 int rev(va_list arg, indicator *func_ptr)
 {
 	char *str =  va_arg(arg, char *);
-	int i, j = 0;
+	int i, j;
 
 	(void)func_ptr;
 	if (!str)
-		;
+		str = "(null)";
 
 	i = _strlen(str);
 	j = i - 1;
